8d1285bdbe0df8c0b2adf678ec32e039199db4d6
Asta Baeck <asta.baeck@gmail.com>
2013-07-15 18:48:58 +0000

    modified file reading code

--- web.js
+++ web.js
@@ -1,11 +1,19 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var buff = new Buffer(5);
+app.get('/', function(request, response) {
+
+var buff = new Buffer();
 buff = fs.readFileSync('index.html');
 var string1 = buff.toString();
-console.log(string1 + "\n");
+response.send(string1);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
