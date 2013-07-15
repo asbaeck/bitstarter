89adbab882d91b81efe8ffc6d5046dca5079c25e
Asta Baeck <asta.baeck@gmail.com>
2013-07-15 17:31:45 +0000

    Updated web.js with reading file

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+var buff = new Buffer(5);
+buff = fs.readFileSync('index.html');
+var string1 = buff.toString();
+console.log(string1 + "\n");
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
