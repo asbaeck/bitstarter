07e007062791a1d5b11e78db9f30c6cca1dbd63f
Asta Baeck <asta.baeck@gmail.com>
2013-07-17 13:08:48 +0000

    web port changed

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
 
@@ -10,12 +10,7 @@ var string1 = buff.toString();
 response.send(string1);
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
