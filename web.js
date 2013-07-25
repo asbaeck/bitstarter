3790ba474bbab55cef780c93b2d408db6f9f4aee
Asta Baeck <asta.baeck@gmail.com>
2013-07-25 18:53:10 +0000

    Port

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
+app.use(express.logger());
+
 app.get('/', function(request, response) {
 
 var buff = new Buffer(50);
@@ -14,3 +16,4 @@ var port = process.env.PORT || 5000
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
