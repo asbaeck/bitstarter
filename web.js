94e1b588b9c898b9fd101826dcead4acf8263dce
Asta Baeck <asta.baeck@gmail.com>
2013-08-02 07:47:59 +0000

    updated

--- web.js
+++ web.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var app = express();
+var htmlfile ="index.html";
 
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-var buff = new Buffer(50);
-buff = fs.readFileSync('index.html');
-var string1 = buff.toString();
-response.send(string1);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 8080
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
